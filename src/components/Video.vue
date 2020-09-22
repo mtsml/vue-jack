599abf62ef2bb4e80399b4bbe949293474e464c4
matsu <50922910+mtsml@users.noreply.github.com>
2020-09-22 14:37:32 +0900

    動画を埋め込み

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -5,20 +5,35 @@
                 {{video_nm}}
             </a>
         </h1>
+        <mdb-container>            
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe 
+                    class="embed-responsive-item" 
+                    v-bind:src="'https://www.youtube.com/embed/'+video_id" 
+                    allowfullscreen
+                >
+                </iframe>
+            </div>
+        </mdb-container>
     </div>
 </template>
 
 <script>
+import { mdbContainer } from 'mdbvue';
 import client from '../api/api'
 
 export default {
     name: 'Video',
+    components: {
+        mdbContainer
+    },
     data: () => ({
         video_id: null,
         video_nm: null
     }),
     mounted() {
         const video_id = this.$route.params.video_id
+        this.video_id = video_id
         client.getVideoInfo(video_id)
             .then(response => this.video_nm = response.data.video_nm)
             .catch(error => alert(error))
