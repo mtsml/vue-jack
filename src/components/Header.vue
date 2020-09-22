59c59f673fbd5056c01eb1396f5b29cec79dda46
matsu <50922910+mtsml@users.noreply.github.com>
2020-09-22 14:07:28 +0900

    ヘッダーおしゃれ化

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="hello">
-        <a href="/">{{ head }}</a>
-    </div>
+    <mdb-navbar color="black" class="z-depth-0" dark>
+         <mdb-navbar-brand href="/">
+            <strong>                
+                {{ head }}
+            </strong>
+        </mdb-navbar-brand>
+    </mdb-navbar>
 </template>
 
 <script>
+import { mdbNavbar, mdbNavbarBrand } from 'mdbvue';
+
 export default {
     name: 'Header',
+    components: {
+        mdbNavbar,
+        mdbNavbarBrand
+    },
     props: {
         head: String
     }
